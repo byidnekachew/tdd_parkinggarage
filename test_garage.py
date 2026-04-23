0d170d24d8a82e195f7fa6df3cf483c8791bcad9
byidnekachew <byidnekachew@my.waketech.edu>
2026-04-23 08:22:39 -0400

    added test for enter garage if duplicate car

--- test_garage.py
+++ test_garage.py
@@ -6,7 +6,14 @@ def test_enter_garage_works():
     enter_garage(garage_dict, "ABC1234", 13)
     assert "ABC1234" in garage_dict["cars"].keys()
 
+
 def test_enter_garage_valerr_if_full():
     with pytest.raises(ValueError):
         garage_dict = {"capacity": 1, "cars": {"ABC1234": 13}}
         enter_garage(garage_dict, "XYZ5678", 15)
+
+
+def test_enter_garage_valerr_if_car_exists():
+    with pytest.raises(ValueError):
+        garage_dict = {"capacity": 10, "cars": {"ABC1234": 13}}
+        enter_garage(garage_dict, "ABC1234", 15)
