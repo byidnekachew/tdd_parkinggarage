86180bb41c741fdb23e5b11d45ebfb94fcd982a6
byidnekachew <byidnekachew@my.waketech.edu>
2026-04-23 08:16:32 -0400

    added valerr test for garage capacity

--- test_garage.py
+++ test_garage.py
@@ -4,3 +4,8 @@ def test_enter_garage_works():
     garage_dict = {"capacity": 10, "cars": {}}
     enter_garage(garage_dict, "ABC1234", 13)
     assert "ABC1234" in garage_dict["cars"].keys()
+
+def test_enter_garage_valerr_if_full():
+    with pytest.raises(ValueError):
+        garage_dict = {"capacity": 1, "cars": {"ABC1234": 13}}
+        enter_garage(garage_dict, "XYZ5678", 15)
