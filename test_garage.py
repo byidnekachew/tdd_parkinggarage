b418da88cafbf9a04d8ce23d18066c2820b15037
byidnekachew <byidnekachew@my.waketech.edu>
2026-04-23 07:58:53 -0400

    added enter garage basic test

--- test_garage.py
+++ test_garage.py
@@ -1,1 +1,6 @@
+from garage import enter_garage
 
+def test_enter_garage_works():
+    garage_dict = {"capacity": 10, "cars": {}}
+    enter_garage(garage_dict, "ABC1234", 13)
+    assert "ABC1234" in garage_dict[cars].keys()
